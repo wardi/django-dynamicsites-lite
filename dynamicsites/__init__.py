8e0cbbf1f6e902f33c59041762183714b91a778b
gemma <none@none>
2011-08-16 02:13:08 +0100

    Loose framework of setup.py
    --HG--
    extra : transplant_source : %0F%27%0Df%D3%F32%89%EE%90%E4%03%DF%7E%2B%10%93H%B5%E2

---
+++ dynamicsites/__init__.py
@@ -0,0 +1,5 @@
+VERSION = (0, 1, 0, 'alpha', 0)
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    return version
---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from distutils.core import setup
+
+version = __import__('dynamicsites').get_version()
+
+setup(
+    name='django-dynamicsites',
+    version=version,
+    description="Host multiple sites from a single django project",
+    long_description="""Expands the standard django.contrib.sites package to allow for:
+    Sites identified dynamically from the request via middleware
+    No need for multiple virtual hosts at the webserver level
+    301 Redirects to canonical hostnames
+    Allows for a site to support multiple subdomains
+    Allows for a site to be an independent subdomain of another site
+    A site may have its own urls.py and templates
+    A single site may accept requests from multiple hostnames
+    Allows for environment hostname mappings to use non-production hostnames (for use in dev, staging, test, etc. environments)""",
+    author='UYSRC',
+    author_email='mtrier@gmail.com',
+    maintainer='Bas van Oostveen',
+    maintainer_email='v.oostveen@gmail.com',
+    url='https://bitbucket.org/ladyrassilon/django-dynamicsites',
+    license='Free as in Beer',
+    platforms=['any'],
+)
