109386d55fd0e74bd07b83c81bab3cb333878c25
crass <crass@berlios.de>
2011-10-12 03:04:57 -0500

    Make automatic folder name filler more sane. If user has edited the folder name field, do not auto-fill it. Try to guess if user has edited the field in the past.
    --HG--
    extra : transplant_source : %02%5B%10%0Bs%7C%E8%9D%EA%DA%B2%C8%FE%2A%F8%03%F9l8%A6

--- dynamicsites/static/js/dynamicsites/admin.js
+++ dynamicsites/static/js/dynamicsites/admin.js
@@ -10,19 +10,33 @@ django.dynamicsites = {
 }
 
 django.jQuery(document).ready(function() {
-    django.dynamicsites.folderNameCheck = django.jQuery('#id_folder_name').val();
-    if (!django.dynamicsites.folderNameCheck) {
-        // 1. if there's nothing in folderName, attach event listener to 
-        //    domain to update folderName dynamically, until the user may edit 
-        //    folderName
-        django.jQuery('#id_domain').change(function() {
-            var folder_name = django.jQuery('#id_folder_name').val();
-            if(folder_name == django.dynamicsites.folderNameCheck) {
-                django.jQuery('#id_folder_name').val(django.dynamicsites.domainToFolderName());
+    var domainEl = django.jQuery('#id_domain');
+    var folderNameEl = django.jQuery('#id_folder_name');
+    django.dynamicsites.domainCheck = domainEl.val();
+    django.dynamicsites.folderNameCheck = folderNameEl.val();
+    if (django.dynamicsites.domainToFolderName() != folderNameEl.val()) {
+        django.dynamicsites.folderNameEdited = true;
+    }
+    
+    // Mark folder name as having been edited when its changed.
+    folderNameEl.change(function() {
+        // If the name is the same as the transformed domain, assume the
+        // user did not edit the field, otherwise assume it was.
+        if (folderNameEl.val() != django.dynamicsites.domainToFolderName()) {
+            django.dynamicsites.folderNameEdited = true;
+        }
+    });
+    
+    // If the folder name has not been edited, auto-generate folder name from
+    // domain name as a convenience.
+    if (!django.dynamicsites.folderNameEdited) {
+        domainEl.change(function() {
+            var folder_name = folderNameEl.val();
+            // If the folder name has not been edited, then set to transformed
+            // domain.
+            if (!django.dynamicsites.folderNameEdited) {
+                folderNameEl.val(django.dynamicsites.domainToFolderName());
             }
         });
-        // 2. if there's nothing in folderName and something in domain, 
-        //    autopopulate folderName
-        django.jQuery('#id_folder_name').val(django.dynamicsites.domainToFolderName());
     }
 });
