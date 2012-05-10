457699cad11f6e8841bfa57fdfa26a8203d9e6d7
Ian Ward <ian@excess.org>
2012-05-10 15:23:12 -0400

    release version 0.1

--- setup.py
+++ setup.py
@@ -5,23 +5,19 @@ from distutils.core import setup
 version = __import__('dynamicsiteslite').get_version()
 
 setup(
-    name='dynamicsiteslite',
+    name='django-dynamicsites-lite',
     version=version,
     description="Host multiple sites from a single django project",
     long_description="""Expands the standard django.contrib.sites package to allow for:
     Sites identified dynamically from the request via middleware
     No need for multiple virtual hosts at the webserver level
     301 Redirects to canonical hostnames
-    Allows for a site to support multiple subdomains
-    Allows for a site to be an independent subdomain of another site
     A site may have its own urls.py and templates
     A single site may accept requests from multiple hostnames
     Allows for environment hostname mappings to use non-production hostnames (for use in dev, staging, test, etc. environments)""",
-    author='UYSRC',
-    author_email='mtrier@gmail.com',
-    maintainer='Bas van Oostveen',
-    maintainer_email='v.oostveen@gmail.com',
-    url='https://bitbucket.org/ladyrassilon/django-dynamicsites',
+    author='Ian Ward',
+    author_email='ian@excess.org',
+    url='https://bitbucket.org/wardi/django-dynamicsites-lite',
     platforms=['any'],
-    packages=['dynamicsites'],
+    packages=['dynamicsiteslite'],
 )
